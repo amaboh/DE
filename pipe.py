f4c095fcd19ac0a5686b726e64fd84c54797c788
amaboh <amabocanon@gmail.com>
2022-12-21 22:36:16 +0000

    ET pipe

--- pipe.py
+++ pipe.py
@@ -19,5 +19,35 @@ df = pd.DataFrame.from_dict(response_list)
 df_columns = ['budget', 'genres', 'id', 'imdb_id', 'orginal_title', 'release_date', 'revenue', 'runtime']
 
 
+genres_list = df['genres'].tolist()
 
 
+flat_list = [item for sublist in genres_list for item in sublist]
+
+result = []
+
+for l in genres_list:
+    r = []
+    for d in l:
+        r.append(d['name'])
+    result.append(r)
+df = df.assign(genres_all= result)
+
+df_genres = pd.DataFrame.from_records(flat_list).drop_duplicates()
+
+
+df_genres_columns = df_genres['name'].to_list()
+df_columns.extend(df_genres_columns)
+
+s = df['genres_all'].explode()
+df = df.join(pd.crosstab(s.index, s))
+
+
+df['release_date'] = pd.to_datetime(df['release_date'])
+df['day'] = df['release_date'].dt.day
+df['month'] = df['release_date'].dt.month
+df['year'] = df['release_date'].dt.year
+df['day_of_work'] = df['release_date'].dt.day_name()
+df_time_columns = ['id', 'release_date', 'day', 'month', 'year', 'day_of_work']
+
+df[df_time_columns]
